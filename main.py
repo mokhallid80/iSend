3ae0e205940025280c498d4365806e30ec0ef2b6
mokhallid80 <mokhallid72@gmail.com>
2023-12-02 02:00:56 +0200

    using .yaml file for configs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import yaml
 from colorama import Fore, Style
 from scapy.all import IP, TCP, sr1
 
@@ -11,8 +12,28 @@ SUCCESS_COLOR = Fore.GREEN
 ERROR_COLOR = Fore.RED
 INFO_COLOR = Fore.BLUE
 
+# print in colors
+def c_print(string, color = Fore.BLUE):
+	print(color + string + Style.RESET_ALL)
+
+# default values
+try:
+	with open("./configs.yaml", "r") as f:
+		configs = yaml.safe_load(f)
+		dst = configs["CONFIGS"]["dst"]
+		dport = configs["CONFIGS"]["dport"]
+		
+except Exception as e:
+	c_print(f"Error - {str(e)}\n", ERROR_COLOR)
+	dst="127.0.0.1"
+	dport=5000
+
+c_print(f"Loaded default values")
+c_print(f"dst {dst}")
+c_print(f"dport {dport}\n")
+
 # create a packet with customized dst and dport(with default values)
-def create_packet(dst="127.0.0.1", dport=5000):
+def create_packet(dst=dst, dport=dport):
 	return IP(dst=dst)/TCP(dport=dport)
 
 # send and wait for the response of the packet we created with a timeout of 2s
@@ -37,10 +58,6 @@ def handle_response(response):
 		is_ack_successfully(response)
 	else:
 		c_print(f"No Response for #{packet_num}", ERROR_COLOR)
-
-# print in colors
-def c_print(string, color = Fore.BLUE):
-	print(color + string + Style.RESET_ALL)
 
 # looping and sending packets
 packet_num = 1
